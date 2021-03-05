6c6aec0a05b7cf9d707328c8916543901cd0a36f
siberia0015 <18965375150@163.com>
2021-03-05 14:52:07 +0800

    test

--- src/main/java/auto/test/wordcount/executor/PythonExecutor.java
+++ src/main/java/auto/test/wordcount/executor/PythonExecutor.java
@@ -1,10 +1,14 @@
 package auto.test.wordcount.executor;
 
+import java.io.IOException;
+
+import static auto.test.wordcount.utils.CmdUtil.cmd;
+
 /**
  * Python执行程序
  *
  * @author <a href="mailto:410486047@qq.com">Grey</a>
- * @date 2021/3/2
+ * @date 2021/3/5
  * @since
  */
 public class PythonExecutor implements Executor {
@@ -15,6 +19,14 @@ public class PythonExecutor implements Executor {
 
     @Override
     public void exec(String src, String input) {
-        // TODO
+        try {
+            if (null != input && !input.trim().equals("")) {
+                cmd("python " + src + " " + input);
+            } else {
+                cmd("python " + src);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
