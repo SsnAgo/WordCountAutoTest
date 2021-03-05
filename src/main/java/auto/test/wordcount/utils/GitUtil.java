6015153675f0efada18b5f527aef701b6aa4dd92
GreyZeng <410486047@qq.com>
2021-03-05 21:47:06 +0800

    remove useless pacakge

--- src/main/java/auto/test/wordcount/utils/GitUtil.java
+++ src/main/java/auto/test/wordcount/utils/GitUtil.java
@@ -4,7 +4,6 @@ package auto.test.wordcount.utils;
 import org.eclipse.jgit.api.CloneCommand;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.transport.UsernamePasswordCredentialsProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
